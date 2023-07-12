b5427eb84728427f1e67f3785702acda9b293ac4
TyT-xexebe <123269617+TyT-xexebe@users.noreply.github.com>
2023-07-12 20:03:21 +0300

    Update HightLighting-Errors.mjs

--- M-logDebugger/scripts/HightLighting-Errors.mjs
+++ M-logDebugger/scripts/HightLighting-Errors.mjs
@@ -30,7 +30,7 @@ for(let iteration1 = 0; iteration1 < lines.length; iteration1++){
 				Errors.push({notfound: firstWord, message: "this command not found in 'keyCommands'", line: iteration1});
 				words[0] = `<span id="errors">${words[0]}</span>`
 			}else{
-			words[0] = `<span id="label">${words[0]}</span>`
+				words[0] = `<span id="label">${words[0]}</span>`
 			}
 		}else{
 				if(words[0] == 'jump'){
