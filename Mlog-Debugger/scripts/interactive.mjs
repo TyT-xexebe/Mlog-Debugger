0bf0e064d5f51913a25fbe334075bd6c9b450bac
TyT-xexebe <123269617+TyT-xexebe@users.noreply.github.com>
2023-08-02 21:30:50 +0300

    Update interactive.mjs

--- Mlog-Debugger/scripts/interactive.mjs
+++ Mlog-Debugger/scripts/interactive.mjs
@@ -5,7 +5,7 @@ const textarea = document.getElementById('codeInput');
 const code = textarea.value.trim();
 const lines = code.split('\n');
 
-loop:let syntaxHelper = () => {
+let syntaxHelper = () => {
   let cursorPos = textarea.selectionStart;
   let text = textarea.value;
   let startOfLine = text.lastIndexOf('\n', cursorPos - 1) + 1;
@@ -16,7 +16,7 @@ loop:let syntaxHelper = () => {
   let lineText = text.substring(startOfLine, endOfLine);
 	console.log(lineText)
 	
-  for (let iteration1 = 0; iteration1 < lines.length; iteration1++){
+  loop:for(let iteration1 = 0; iteration1 < lines.length; iteration1++){
 		let words = lineText[iteration1].split(' ');
     firstWord = words[0];
     secondWord = words[1];
